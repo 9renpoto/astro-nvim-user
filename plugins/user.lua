c5175337efdfac91ad5d132649e7b3d2ea0e52f1
Keisuke Umeno <9renpoto@gmail.com>
2023-12-31 15:48:37 +0900

    feat: enable rust_analyzer

--- plugins/user.lua
+++ plugins/user.lua
@@ -12,4 +12,13 @@ return {
   {
     "wakatime/vim-wakatime",
   },
+  {
+    "simrat39/rust-tools.nvim",
+    {
+      "williamboman/mason-lspconfig.nvim",
+      opts = {
+        ensure_installed = { "rust_analyzer" },
+      },
+    },
+  },
 }
